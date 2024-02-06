863affd583a7f499e581549fd5bd193f433245e4
tiwariakshat47 <atiwari5@ucsc.edu>
2024-02-05 23:12:43 -0800

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,4 @@
 # IMPORT LIBRARIES
-#pip install beautifulsoup4
 from bs4 import BeautifulSoup
 import requests
 
@@ -12,37 +11,17 @@ soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
 # FIND ALL THE ROWS WITH CLASS 'dark'
 rows = soup.findAll("tr")
 
+# Create a list to store the rows with their modified text
+formatted_rows = []
+
 for row in rows:
     # Replace newline characters and remove leading/trailing whitespaces
     row_text = row.text.replace('\n', '').strip()
+    formatted_rows.append(row_text)
+
+# Sort the rows alphabetically based on the name
+formatted_rows.sort(key=lambda x: x.split()[0])
+
+# Print the sorted rows with the specified styles
+for row_text in formatted_rows:
     print(row_text)
-
-# Set a maximum number of prints
-
-
-# for type, name in zip(types, names):
-#     if print_count < max_prints:
-#         print(name.text + " contains " + type.text)
-#         print_count += 1
-#     else:
-#         break
-
-
-#FIND ALL THE ITEMS IN THE PAGE WITH A CLASS ATTRIBUTE OF 'AUTHOR'
-#AND STORE THE LIST AS A VARIABLE
-# brawlers = soup.findAll(attrs={"class":"rarity4 text-center small mb-1"})
-# # print(brawlers)
-# names = [h3.text.strip() for h3 in soup.find_all('h3')]
-# # print(names)
-# #LOOP THROUGH BOTH LISTS USING THE 'ZIP' FUNCTION
-# #AND PRINT AND FORMAT THE RESULTS
-# print(winrates)
-# i = 1
-# for name in names:
-#     if(i > 76):
-#         break
-#     print(name)
-#     i = i+1
-    
-# for winrate, name in zip(winrates, names):
-#     print(winrate.text + "-" + name)
