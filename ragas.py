92de70a173ab4c14519bdc12e1d4f57d8a02b2d1
tiwariakshat47 <atiwari5@ucsc.edu>
2024-02-06 10:58:56 -0800

    added previous ragas implementation

--- ragas.py
+++ ragas.py
@@ -1,29 +1,45 @@
-import os
-os.environ["OPENAI_API_KEY"] = "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"
+# import os
+# os.environ["OPENAI_API_KEY"] = "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"
 
 
-from datasets import load_dataset
+# from datasets import load_dataset
 
-fiqa_eval = load_dataset("explodinggradients/fiqa", "ragas_eval")
-print(fiqa_eval)
+# fiqa_eval = load_dataset("explodinggradients/fiqa", "ragas_eval")
+# print(fiqa_eval)
 
-from ragas.metrics import (
-    answer_relevancy,
-    faithfulness,
-    context_recall,
-    context_precision,
-)   
+# from ragas.metrics import (
+#     answer_relevancy,
+#     faithfulness,
+#     context_recall,
+#     context_precision,
+# )   
+
+# from ragas import evaluate
+
+# result = evaluate(
+#     fiqa_eval["baseline"].select(range(3)), # selecting only 3
+#     metrics=[
+#         context_precision,
+#         faithfulness,
+#         answer_relevancy,
+#         context_recall,
+#     ],
+# )
+
+# result
 
 from ragas import evaluate
+from datasets import Dataset
+import os
 
-result = evaluate(
-    fiqa_eval["baseline"].select(range(3)), # selecting only 3
-    metrics=[
-        context_precision,
-        faithfulness,
-        answer_relevancy,
-        context_recall,
-    ],
-)
+os.environ["OPENAI_API_KEY"] = "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"
 
-result
+# prepare your huggingface dataset in the format
+# Dataset({
+#     features: ['question', 'contexts', 'answer', 'ground_truths'],
+#     num_rows: 25
+# })
+
+dataset: Dataset
+
+results = evaluate(dataset)
